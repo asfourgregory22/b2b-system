ad6916d9812152e1704b92a99a001a44f59d0f29
asfourgregory <asfourgregory@gmail.com>
2026-08-15 17:12:46 +0300

    Add login function, checkPassword instance method to User model 2nd attempt after saving

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,69 @@
-const user = require("../models/userModel");
+const User = require("../models/userModel");
 const validator = require("validator");
 const  jwt = require("jsonwebtoken");
-const {promisify} = reqiure("util");
+const {promisify} = require("util");
 
 const signToken = (id) => {
-    return jwt.sign( {id}  ,process.env.JWT_SECTRET,{
+    return jwt.sign( {id}  ,process.env.JWT_SECRET,{
         expiresIn: process.env.JWT_EXPIRES_IN,
     })
-}
+};
 
+exports.register = async (req,res) => {
+    try{
+        const { name, email, password, passwordConfirm, role } = req.body
+
+        const newUser = await User.create({ name, email, password, passwordConfirm, role });
+        const token = signToken(newUser._id);
+
+        newUser.password = undefined;
+
+        res.status(201).json({status:'success',
+            token,
+            data : {
+                user : newUser
+            }
+        });
+
+    }catch(err){
+        console.log(err);
+        res.status(400).json({
+            status : "fail",
+            message : err.message
+        });
+    }
+};
+
+exports.login = async (req,res) => {
+    try{
+        const {email,password} = req.body;
+
+        const user = await User.findOne({email:email});
+     
+        if(!user){
+            return res.status(404).json({message:"User not found."});
+        };
+
+        if(!(await user.checkPassword(password,user.password))){
+            return res.status(401).json({message:"Incorrect email or password."});
+        };
+
+        const token = signToken(user._id);
+        user.password = undefined;
+
+        res.status(200).json({
+            status: 'success',
+            token,
+            data : {
+                user
+            }
+        });
+
+    }catch(err){
+        console.log(err);
+        res.status(400).json({
+            status : 'fail',
+            message : err.message
+        });
+    }
+};
--- models/userModel.js
+++ models/userModel.js
@@ -70,4 +70,8 @@ userSchema.pre("save", async function(next){
     }
 })
 
+userSchema.methods.checkPassword = async function(candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
+
 module.exports = mongoose.model("User",userSchema);
