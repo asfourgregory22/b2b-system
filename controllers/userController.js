493d630a1c232450a9d2f66e9f1e1c6d2c835132
asfourgregory <asfourgregory@gmail.com>
2026-08-15 17:11:40 +0300

    Add login function, checkPassword instance method to User model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,11 @@
 const user = require("../models/userModel");
 const validator = require("validator");
-const
+const  jwt = require("jsonwebtoken");
+const {promisify} = reqiure("util");
+
+const signToken = (id) => {
+    return jwt.sign( {id}  ,process.env.JWT_SECTRET,{
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    })
+}
+
