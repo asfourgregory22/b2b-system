b55e74e26f204f8ae735ef9cb3cdeb7b9573c3f8
asfourgregory <asfourgregory@gmail.com>
2026-08-26 11:21:42 +0300

    Add Dashboard with user info and logout

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,23 @@
+import { useAuth } from '../context/useAuth'
+import { useNavigate } from 'react-router-dom'
+
 function Dashboard() {
-    return <h1>Dashboard Page</h1>
+    const { user , logout } = useAuth()
+    const navigate = useNavigate()
+
+    function handleLogout() {
+        logout()
+        navigate('/')
+    }
+
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>Welcome, {user.name}</p>
+            <p>Role : {user.role}</p>
+            <button onClick={handleLogout}>Log Out</button>
+        </div>
+    )
 }
 
 export default Dashboard
